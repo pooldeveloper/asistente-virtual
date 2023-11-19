99bf0203ac4a855a3c87f5d8175189b74876f95b
Pool Nuñez <74687129+pooldeveloper@users.noreply.github.com>
2023-11-19 08:19:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-from flask import Flask, jsonify
 import os
+from pathlib import Path
+from openai import OpenAI
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from playsound import playsound
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template("recorder.html")
 
+@app.route("/audio", methods=["POST"])
+def audio():
+    audio = request.files.get("audio")
+    audio.save("audio.mp3")
+    audio_file = open("audio.mp3", "rb")
+
+    transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+
+    stream = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "Eres un asistente malhablado"},
+            {"role": "user", "content": transcription.text},
+        ],
+        model="gpt-3.5-turbo",
+        stream=True
+    )
+
+    result = ""
+    for part in stream:
+        result += part.choices[0].delta.content or ""
+
+    response = client.audio.speech.create(model="tts-1", voice="alloy", input=result)
+
+    response.stream_to_file("output.mp3")                                                           
+
+    playsound("output.mp3")
+    
+    return {"result": "ok", "text": result}
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
